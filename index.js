386a59ee87f6d6c266d4eef61edd6faddaec50fb
maribaaaa <vimaribaboare@gmail.com>
2024-11-05 14:13:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let firstJavaCode = "Hello World";
+console.log (firstJavacode);
+
+var age=20;
+
+//We are coding the Degree to Farenheit Temperation Conversion
+/*multiple
+line
+comment*/
+
+var degreeCelcious = 10
+var degreeFarenheit;  
+    TemperatureResult = (degreeCelcious * (9/5)) + 32;
+
+
+let isEqual = (10 == 10);
+let danielHeight = 6;
+let chrisHeight = 5.5;
+let gaint = 6;
+let isTaller = (danielHeight == chrisHeight);
+let isTallest = (danielHeight == gaint);
+
+
+
+function calculateArea(width, height){
+   
+       area = width * height;
+       
+
+       return area;
+}
+
+calculateArea(2,8);
+
+
+
+
+
+
